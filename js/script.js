8783c1b6780ff245230e8619732b089d7988b7d9
GiulianoCretella <giulianocretella.1@gmail.com>
2022-05-25 16:33:25 +0200

    bonus filtrare per genere completo

--- js/script.js
+++ js/script.js
@@ -2,14 +2,32 @@ const app= new Vue({
     el:'#app',
     data:{
         albums:[],
-        apiPath:'./server.php'
+        apiPath:'./server.php',
+        genereSelezionato:'',
+        generi:[],
     },
     methods:{
-
+        cambiaGenere(){
+            this.axCall(this.genereSelezionato);
+        },
+        axCall(genere=null){
+            path = this.apiPath;
+            if(genere){
+                path= `${this.apiPath}?genere=${genere}`
+            }
+            axios.get(path).then((res)=>{
+                this.albums=res.data;
+                if(this.generi < 1){
+                    this.albums.forEach((album)=>{
+                        if(!this.generi.includes(album.genre)){
+                            this.generi.push(album.genre)
+                        }
+                    })
+                }
+            })
+        }
     },
     mounted(){
-        axios.get(this.apiPath).then((res)=>{
-            this.albums=res.data;
-        })
+        this.axCall();
     }
 })
--- server.php
+++ server.php
@@ -1,4 +1,14 @@
 <?php include __DIR__.'/database.php';
+$switchResult=[];
+if(isset($_GET['genere'])){
+    foreach($albums as $album){
+        if($album['genre'] == $_GET['genere']){
+            $switchResult[]= $album;
+        };
+    };
+}else{
+    $switchResult = $albums;
+};
 header('Content-Type: application/json');
-echo json_encode($albums)
+echo json_encode($switchResult)
 ?>
